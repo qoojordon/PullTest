c7808434d2197c17cc9818e3f02367060fa157e8
RyanLai@iMac <qoojordon@gmail.com>
2020-04-05 17:01:39 +0800

    Initial Commit

---
+++ PullTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PullTest
+//
+//  Created by Ryan Lai on 2020/4/5.
+//  Copyright © 2020 Ryan Lai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
